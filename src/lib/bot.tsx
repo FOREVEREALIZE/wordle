16d7b799fd6c52759263301236d01a0ab83205b5
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-05-05 13:27:38 +0200

    work

--- src/lib/bot.tsx
+++ src/lib/bot.tsx
@@ -1,8 +1,12 @@
 import { WORDS } from '../constants/wordlist'
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 function getGuess(pattern: RegExp) {
     const validWords = WORDS.filter((word) => word.match(pattern))
-    return(validWords[Math.random(validWords.length)])
+    return(validWords[getRandomInt(validWords.length)])
 }
 
 export default {
